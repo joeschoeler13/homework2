7c37b57037aefad01c0b6ff354cebab82c222e33
Neuer Nutzer <neuernutzer@Marcuss-MacBook-Pro.local>
2017-09-15 02:54:50 +0200

    This is my second homework, containing the .js file with the program that adds three numbers to a sum

---
+++ homework_2_program.js
@@ -0,0 +1,6 @@
+alert("The following programm will let you insert THREE numbers and add them to a sum.")
+var x = +prompt("Please enter your first number!")
+var y = +prompt("Please enter your second number!")
+var z = +prompt("Please enter your third number!")
+a = x + y + z
+alert("The sum is " + a + ".")
